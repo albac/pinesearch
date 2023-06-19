fc632108dcc7f769e132b2c7e1f2ccfe9cbdbf8a
Alfredo Baldoceda <alfrds@gmail.com>
2023-06-19 11:21:56 -0400

    nexjs app route enable

--- next.config.js
+++ next.config.js
@@ -1,4 +1,23 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    appDir: true,
+  },
+  env: {
+    USER_BRANCH: process.env.USER_BRANCH,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pineblogs101145-dev.s3.us-west-2.amazonaws.com",
+        port: "",
+        pathname: "/public/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
