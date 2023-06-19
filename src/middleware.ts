ebcec016dc73f4d8ced2fba3b933250d46c62ca9
isaul19 <saulhola348@gmail.com>
2023-06-19 13:15:59 -0500

    problem 'not found'

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,24 @@
-import { authMiddleware } from "@clerk/nextjs";
+import { authMiddleware, redirectToSignIn } from "@clerk/nextjs";
+// import { getLastPath } from "./helpers";
+
+const publicRoutes = ["/", "/auth/sign-in", "/auth/sign-up", "/auth/sign-out"];
+// const privateRoutes = ["/example-private"];
 
 export default authMiddleware({
+  // afterAuth(auth, req, evt) {
+  //   const path = getLastPath(req.url);
+  //   if (!publicRoutes.includes(path) && !privateRoutes.includes(path)) {
+  //     return redirectToSignIn({ returnBackUrl: "http://localhost:3000/" });
+  //     return notFound()
+  //   }
+
+  //   if (!auth.userId && !auth.isPublicRoute) {
+  //     return redirectToSignIn({ returnBackUrl: req.url });
+  //   }
+  // },
   beforeAuth: function () {},
-  publicRoutes: ["/"]
+  publicRoutes: publicRoutes,
+  ignoredRoutes: []
 });
 
 export const config = {
