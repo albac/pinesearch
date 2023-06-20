0cfc9e7ec23352fb5207a67741a1c28f76ba3357
isaul19 <saulhola348@gmail.com>
2023-06-19 22:35:11 -0500

    added clerk variables in next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,16 @@ const nextConfig = {
   env: {
     USER_BRANCH: process.env.USER_BRANCH,
     OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY:
+      process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+    NEXT_PUBLIC_CLERK_SIGN_IN_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_SIGN_OUT_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_OUT_URL,
+    NEXT_PUBLIC_CLERK_SIGN_UP_URL: process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL,
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL:
+      process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_IN_URL,
+    NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL:
+      process.env.NEXT_PUBLIC_CLERK_AFTER_SIGN_UP_URL
   },
   images: {
     remotePatterns: [
@@ -11,10 +21,10 @@ const nextConfig = {
         protocol: "https",
         hostname: "pineblogs101145-dev.s3.us-west-2.amazonaws.com",
         port: "",
-        pathname: "/public/**",
-      },
-    ],
-  },
+        pathname: "/public/**"
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
