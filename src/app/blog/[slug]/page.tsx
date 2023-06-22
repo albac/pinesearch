8402b7c31c4b6ca3fc048930d6c1a9919371a866
isaul19 <saulhola348@gmail.com>
2023-06-22 13:24:37 -0500

    added generate metadata by title and summary

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,21 +1,53 @@
-import { Post } from "@/models";
+import { Post as PostModel } from "@/models";
 import { SortDirection, Storage, withSSRContext } from "aws-amplify";
 import { MDXRemote } from "next-mdx-remote/rsc";
 
-export async function generateStaticParams() {
+interface StaticParams {
+  slug: string;
+}
+
+interface BlogPageParams {
+  params: {
+    slug: string;
+  };
+}
+
+interface Post {
+  title: string;
+  summary: string;
+  s3url: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+export async function generateStaticParams(): Promise<StaticParams[]> {
   const { DataStore } = withSSRContext();
-  const posts = await DataStore.query(Post, (c: any) => c, {
+  const posts = await DataStore.query(PostModel, (c: Post) => c, {
     sort: (s: any) => s.updatedAt(SortDirection.DESCENDING)
   });
 
-  return posts.map((post: any) => ({
+  return posts.map((post: Post) => ({
     slug: post.s3url
   }));
 }
 
-export const revalidate = 30;
+export async function generateMetadata({ params }: BlogPageParams) {
+  const { DataStore } = withSSRContext();
+  const post = await DataStore.query(PostModel, (c: any) => c.s3url.eq(params.slug));
 
-export default async function blogPage({ params }: { params: any }) {
+  if (!post || !post[0]?.title) {
+    return null;
+  }
+
+  return {
+    title: post[0].title,
+    description: post[0].summary
+  };
+}
+
+export const revalidate: number = 30;
+
+export default async function blogPage({ params }: BlogPageParams) {
   const file = await Storage.get(`${params.slug}.mdx`, {
     level: "public"
   });
