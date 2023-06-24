d6ba25ed01e0688b19272cd06f439659afb16d4c
isaul19 <saulhola348@gmail.com>
2023-06-24 12:37:35 -0500

    added new logo

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -8,7 +8,10 @@ export default async function Navbar() {
   return (
     <header className="w-full shadow-sm z-10 relative bg-white">
       <nav className="mx-auto w-11/12 max-w-[1400px] justify-between py-7 sm:flex">
-        <Link href="/" className="text-center text-2xl font-bold flex items-center gap-2">
+        <Link
+          href="/"
+          className="text-center text-2xl font-bold flex items-center justify-center gap-2"
+        >
           <StarLogoIcon />
           <span>PineSearch</span>
         </Link>
