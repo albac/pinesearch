755b33fbaf64270643c7032923ecda2032f83cc1
isaul19 <saulhola348@gmail.com>
2023-06-24 20:17:33 -0500

    added search model, and responsive

--- src/app/chat/SearchResult.tsx
+++ src/app/chat/SearchResult.tsx
@@ -9,19 +9,19 @@ interface ISearchResultProps {
 export default function SearchResult({ queries, result }: ISearchResultProps) {
   return (
     <>
-      <div className="flex flex-col w-5/6 justify-center p-8 mt-2 border-b border-slate-100">
-        <div className="flex w-full items-center">
+      <div className="flex flex-col w-5/6 justify-center pt-5 sm:p-8 mt-2 border-b border-slate-100">
+        <div className="sm:flex w-full items-center">
           <div className="w-10 h-10 rounded-full border border-fig-gray flex justify-center items-center ">
             <Image src="/icons/user.svg" alt="user" width="20" height="20" />
           </div>
-          <p className="ml-4 w-5/6 text-left">{queries[queries.length - 1]}</p>
+          <p className="sm:ml-4 mt-2 sm:mt-0 w-5/6 text-left">{queries[queries.length - 1]}</p>
         </div>
-        <div className="flex w-full mt-8">
+        <div className="sm:flex w-full mt-8 mb-5">
           <Image src="/icons/AI.svg" alt="user" width="40" height="40" className="h-10" />
-          <p className="ml-4 w-full text-left">{result}</p>
+          <p className="sm:ml-4 mt-2 sm:mt-0 w-full text-left">{result}</p>
         </div>
       </div>
-      <div className="w-5/6 px-8 flex items-start my-4">
+      <div className="w-5/6 sm:px-8 flex items-start my-4">
         <h3 className="font-semibold uppercase text-xs text-fig-gray">
           Your Question is Answered In These Posts
         </h3>
