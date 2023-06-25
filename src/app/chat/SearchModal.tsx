fed56e120e5ff5123bb26928ac1e5348cae35b95
isaul19 <saulhola348@gmail.com>
2023-06-25 11:49:49 -0500

    hotfix type

--- src/app/chat/SearchModal.tsx
+++ src/app/chat/SearchModal.tsx
@@ -47,7 +47,7 @@ export default function SearchModal({ closeModal }: ISearchModalProps) {
         @param text - optional - the query when a user has clicked on
         an example search.
     */
-  const onSubmitSearch = async (e: React.FormEvent, text?: string) => {
+  const onSubmitSearch = async (e?: React.FormEvent, text?: string) => {
     e.preventDefault();
 
     try {
