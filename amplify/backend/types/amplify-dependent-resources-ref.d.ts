df9bf332e05db8d6c0711b35ac655014bc3b887f
Alfredo Baldoceda <alfrds@gmail.com>
2023-09-14 15:19:30 -0400

    fixed issues

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -15,8 +15,10 @@ export type AmplifyDependentResourcesAttributes = {
     "pinesearchc776f9f1": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
+      "HostedUIDomain": "string",
       "IdentityPoolId": "string",
       "IdentityPoolName": "string",
+      "OAuthMetadata": "string",
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,17 @@ import "./globals.css";
 import Navbar from "./Navbar";
 import AuthenticatorProvider from "@/components/AuthenticatorProvider";
 
+if (process.env.USER_BRANCH === "prod") {
+  awsconfig.oauth.redirectSignIn = "https://albac.dev/";
+  awsconfig.oauth.redirectSignOut = "https://albac.dev/";
+} else if (process.env.USER_BRANCH === "stage") {
+  awsconfig.oauth.redirectSignIn = "https://beta.albac.dev/";
+  awsconfig.oauth.redirectSignOut = "https://beta.albac.dev/";
+} else {
+  awsconfig.oauth.redirectSignIn = "http://localhost:3000/";
+  awsconfig.oauth.redirectSignOut = "http://localhost:3000/";
+}
+
 Amplify.configure({ ...awsconfig, ssr: true });
 
 export const metadata = {
